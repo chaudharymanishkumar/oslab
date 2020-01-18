4596214d700c70ca69b0f6e32fa600f6ddf9873b
Manish Kumar Chaudhary <42311706+chaudharymanishkumar@users.noreply.github.com>
2020-01-18 19:40:57 +0530

    Add files via upload
    Transferring n numbers from parent process to child process

---
+++ nnumber_pipe.py
@@ -0,0 +1,29 @@
+import os , sys
+import time
+
+num=""
+n=input("Enter no of numbers:")
+r,w =os.pipe()
+pid1=os.fork()
+if pid1:
+	print "parent"
+	os.close(r)
+	w=os.fdopen(w,'w')
+	print "parent is writing"
+	for i in range(n):
+	        n=input("Enter number:")
+        	num=num+ str(n) +" "
+	w.write(num)
+	w.close()
+	sys.exit(0)
+
+else:
+	time.sleep(5)
+	print("\nchild")
+	os.close(w)
+	r=os.fdopen(r)
+	print("child is reading")
+	str=r.read()
+	print "number is:", str
+	r.close()
+	sys.exit(0)
